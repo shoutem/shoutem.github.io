ae7756fd51adcdd159048ff1e0c7c46dc743b836
Domagoj Lazic <domlazic@gmail.com>
2016-05-24 13:38:19 +0200

    add footer to documentation

--- js/main.js
+++ js/main.js
@@ -9,3 +9,44 @@ function closeMenu() {
     if (window.event.target.nodeName !== 'A')
         overlay.classList.remove('open');
 }
+$(function() {
+	
+	function onFooterResize() {
+		// throttle
+		setTimeout(function() {
+			$("#wrapper").css({ marginBottom: ($(".footer").height() + $('.pager').outerHeight()) + "px" });
+		}, 300);
+	}
+	
+	$(window).on("resize", onFooterResize);
+	onFooterResize();
+	
+	// header
+	
+	var headroom  = new Headroom($("nav.headroom").get(0), 
+		{
+			tolerance: {
+				down : 10, up : 20
+			},
+			offset : 81
+		}	
+	);
+	headroom.init();
+	
+	// navbar
+	
+	function scrollTo(p_y) {
+		$('html, body').animate({
+			scrollTop: p_y
+		}, 'slow');			
+	}
+	
+	$('a[href="#signup"]').on("click", function(e) {
+		
+		e.preventDefault();
+		$this = $(this);
+		
+		scrollTo($(document).height());
+		
+	});
+});
